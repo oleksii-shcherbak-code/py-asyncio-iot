03cba98b795ab9d1ea9345e0a31fb57cdc7912d8
Oleksii Shcherbak <qksardas001@mail.com>
2026-02-03 14:53:03 +0200

    Fix

--- app/iot/service.py
+++ app/iot/service.py
@@ -11,13 +11,13 @@ def generate_id(length: int = 8) -> str:
 
 
 class Device(Protocol):
-    def connect(self) -> None:
+    async def connect(self) -> None:
         ...
 
-    def disconnect(self) -> None:
+    async def disconnect(self) -> None:
         ...
 
-    def send_message(self, message_type: MessageType, data: str) -> None:
+    async def send_message(self, message_type: MessageType, data: str) -> None:
         ...
 
 
